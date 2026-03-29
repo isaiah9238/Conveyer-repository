759f85faa8e7d332b6d6a16dd86727d39da844fa
Isaiah Smith <isaiah9238@gmail.com>
2026-03-29 22:06:21 +0000

    firbase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "A",
-  authDomain: "studio-2772757020-e1e46.firebaseapp.com",
-  projectId: "studio-2772757020-e1e46",
-  storageBucket: "studio-2772757020-e1e46.firebasestorage.app",
-  messagingSenderId: "53908489351",
-  appId: "1:53908489351:web:f3281b9afe2dc20528a422"
+  apiKey: "FIREBASE_SERVICE_ACCOUNT",
+  NEXT_AUTH_DOMAIN: "studio-2772757020-e1e46.firebaseapp.com",
+  NEXT_PROJECT_ID: "studio-2772757020-e1e46",
+  NEXT_STORAGE_BUCKET: "studio-2772757020-e1e46.firebasestorage.app",
+  NEXT_MESSENGING_ID: "53908489351",
+  NEXT_APP_ID: "1:53908489351:web:f3281b9afe2dc20528a422"
 };
 
 // Initialize Firebase
