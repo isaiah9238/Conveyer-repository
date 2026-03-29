925c9aca589f61f3cd2a89001d549e5c8a3686ec
Isaiah Smith <isaiah9238@gmail.com>
2026-03-29 16:27:35 +0000

    The file has the configuration in it.

---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "A",
+  authDomain: "studio-2772757020-e1e46.firebaseapp.com",
+  projectId: "studio-2772757020-e1e46",
+  storageBucket: "studio-2772757020-e1e46.firebasestorage.app",
+  messagingSenderId: "53908489351",
+  appId: "1:53908489351:web:f3281b9afe2dc20528a422"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
